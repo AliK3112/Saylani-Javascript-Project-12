82268b5782c3351235abf0f75f34e16b5cf1030c
Ali Khalil <83224003+JanuMshkillz@users.noreply.github.com>
2021-12-15 16:56:39 +0500

    initial Javascript

--- app.js
+++ app.js
@@ -21,3 +21,11 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+// console.log(items);
+
+let deadlineDate = new Date(2021, 11, 16, 8, 0, 0);
+// console.log(deadlineDate);
